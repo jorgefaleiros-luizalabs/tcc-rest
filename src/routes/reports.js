394f1996ec8aed6efefd755177de3550978d9a0a
jorgeFaleiros <jorge.faleiros@luizalabs.com>
2017-11-08 10:47:56 -0200

    rota de posiçoes

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var connection = require('../database/connection');
 var moment = require('moment-timezone');
 var controller = require('../controller/reports');
+moment.tz.setDefault('America/Sao_Paulo');
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+const DateFormat = 'YYYY-MM-DD HH:mm:ss'; 
 
+router.get('/', (req, res, next) => {
   connection.connection.query('select * from tcc.tb_reports;', (err, rows, fields) => {
     var bodyResponse = [];
     for (var row in rows) {
@@ -16,6 +18,24 @@ router.get('/', (req, res, next) => {
     res.send(bodyResponse);
   });
 });
+
+router.get('/position', (req, res, next) => {
+  connection.connection.query('select latitude, longitude from tcc.tb_reports where created_at between \'' + moment().add(-7, 'days').format(DateFormat) + '\' and \'' + moment().format(DateFormat) +'\';', (err, rows, fields) => {
+    var bodyResponse = [];
+
+    if (err) throw err;
+
+    if (rows.length === 0){
+      res.status(404);
+    }
+    for (var row in rows) {
+      if (rows.hasOwnProperty(row)) {
+        bodyResponse.push(rows[row]);
+      }
+    }
+    res.send(bodyResponse);
+  });
+})
 
 router.get('/:id', (req, res, next) => {
 
